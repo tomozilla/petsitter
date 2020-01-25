f6d1c8a95683395cabb57bf634dc74aaf5efc714
Paula Kusinska <paula.kusinska@gmail.com>
2020-01-25 13:58:41 +0900

    Added jobs controller methods

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,11 +1,32 @@
 class JobsController < ApplicationController
+  def index
+    @job = Job.all
+  end
+
   def new
-    @job = Job.find(params[:job_id]])
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.find(job_params)
+    if @job.save
+      redirect_to job_path(@job)
+    else
+      render :new
+    end
   end
 
   def edit
+    @job = Job.find(params[:id])
   end
 
   def show
+    @job = Job.find(params[:id])
+   end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:date, :location, :number_of_dogs)
   end
 end
