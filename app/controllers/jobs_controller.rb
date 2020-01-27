84815a77596f37e05059990caccf7964e8d3f6fa
Paula Kusinska <paula.kusinska@gmail.com>
2020-01-27 17:09:54 +0900

    added landing page and card grid

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,29 +1,11 @@
 class JobsController < ApplicationController
   def new
-    @job = Job.new
+    @job = Job.find(params[:job_id]])
   end
 
-  def create
-    @job = Job.find(job_params)
-    if @job.save
-      redirect_to job_path(@job)
-    else
-      render :new
-    end
-  end
-
-  def update
-    @job = Job.find(params[:id])
-    @job.update(job_params)
+  def edit
   end
 
   def show
-    @job = Job.find(params[:id])
-  end
-
-  private
-
-  def job_params
-    params.require(:job).permit(:date, :location, :number_of_dogs)
   end
 end
