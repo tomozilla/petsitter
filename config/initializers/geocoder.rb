f11cb3b431f481a30eee83516308bf4512f6719f
Paula <41625282+Papatya91@users.noreply.github.com>
2020-01-28 20:25:02 +0900

    Update geocoder.rb

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -11,12 +11,12 @@ Geocoder.configure(
   cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
-  Exceptions that should not be rescued by default
-  (if you want to implement custom error handling);
-  supports SocketError and Timeout::Error
-  always_raise: [],
+#   Exceptions that should not be rescued by default
+#   (if you want to implement custom error handling);
+#   supports SocketError and Timeout::Error
+#   always_raise: [],
 
-  Calculation options
-  units: :km,                 # :km for kilometers or :mi for miles
-  distances: :linear          # :spherical or :linear
+#   Calculation options
+#   units: :km,                 # :km for kilometers or :mi for miles
+#   distances: :linear          # :spherical or :linear
 )
